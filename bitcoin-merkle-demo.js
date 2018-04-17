c318364cf900baf1630d54f9ca1638e66738652a
Daniel Mross <dmross@nxtportal.org>
2018-04-17 18:28:09 -0400

    variable names

--- bitcoin-merkle-demo.js
+++ bitcoin-merkle-demo.js
@@ -21,7 +21,7 @@ function hexToByteArr (hex) {
 
 // block 518588
 // big-endian hex
-let transactions = ["46ceb4677d2c95d356105ada337003d8cf8c5df7cd6de7497e9cddb0f60bc2a2",
+let transactionsArr = ["46ceb4677d2c95d356105ada337003d8cf8c5df7cd6de7497e9cddb0f60bc2a2",
 "11e110e547d2880fb80d44c6db120384a8c01955594a6865ace3fbf5ab22386b",
 "07f21a36e6044ac76e53708ee547d1ceee04daddb3240af9210fbfde37b44635",
 "ed16c2e218daab8e00490e43ce64f1fa710568a80e12e263b12e78c729db9f24",
@@ -37,15 +37,16 @@ function merklize (txArr) {
 	
 	console.log("--- " + txArr.length + " nodes ---");
 	console.log(txArr);	
+	
 	while(txArr.length) {
 		// convert hex to little-endian byte arrays
-		let h1 = hexToByteArr(txArr.shift()).reverse();
-		let h2 = txArr.length ? hexToByteArr(txArr.shift()).reverse() : h1;
-		let res = sha256.digest(sha256.digest(h1.concat(h2)));
+		let node1 = hexToByteArr(txArr.shift()).reverse();
+		let node2 = txArr.length ? hexToByteArr(txArr.shift()).reverse() : node1;
+		let res = sha256.digest(sha256.digest(node1.concat(node2)));
 
 		// convert back to big-endian hex
-		let reshex = byteArrToHex(res.reverse());
-		retArr.push(reshex);
+		let resHex = byteArrToHex(res.reverse());
+		retArr.push(resHex);
 	}
 	
 	if(retArr.length === 1) {
@@ -56,7 +57,7 @@ function merklize (txArr) {
 	merklize(retArr);
 }
 
-merklize(transactions);
+merklize(transactionsArr);
 
 console.log(`
                            ------------------ecc7-----
@@ -82,14 +83,14 @@ console.log(`
 46ce  11e1  07f2  ed16  eee4  7509  3ebc  cf36  5bab  4139
 */
 
-// To prove that 5bab is in the block, all we need is 4139 and 5d85
+// To prove that 5bab is in the block, all we need is 5d85 and 4139
 
-let nodesHex  = ["5d85399ada11cfc184f400ec46f2deba9f2a475e70e83d3c34e4eff5e33a3aae",
+let nodesArr = ["5d85399ada11cfc184f400ec46f2deba9f2a475e70e83d3c34e4eff5e33a3aae",
 "5bab4a2f361e22004f62764e7634e166600089d4659756e8b5f3ed677e3a06d8",
 "4139e3d2ea370d085e78025c2a9b36641c8ddc4e38919b7ae6260666d9c476eb"]
 
 // convert hex to little-endian byte arrays
-let nodes = nodesHex.map(n => hexToByteArr(n).reverse());
+let nodes = nodesArr.map(n => hexToByteArr(n).reverse());
 
 let r = sha256.digest(sha256.digest(nodes[1].concat(nodes[2])));
 r = sha256.digest(sha256.digest(r.concat(r)));
